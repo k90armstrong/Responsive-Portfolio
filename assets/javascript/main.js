5064131d693ab9a48cf46ce3e947bdd3629496f0
Kyle <k90armstrong@gmail.com>
2017-09-30 18:18:17 -0600

    mostly done with breakpoints

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,11 @@
 window.addEventListener("scroll", function () {
     var scrollY = document.body.scrollTop;
     // check if larger than height of the top bar
-    if (scrollY >= 40) {
-        document.getElementById("main-header").className = 'main-header rotate fix'  // if it is larger than add the class to flip and move the header
+    if (scrollY >= 10) {
+        console.log('move header')
+        // document.getElementById("main-header").className = 'main-header rotate fix' // if it is larger than add the class to flip and move the header
     } else {
-        document.getElementById("main-header").className = 'main-header' // if it is not remove the class to flip it
+        // document.getElementById("main-header").className = 'main-header' // if it is not remove the class to flip it
+        console.log('move header back')
     }
 })
